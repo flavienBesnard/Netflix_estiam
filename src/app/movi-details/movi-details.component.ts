156b49b6ccc77cc984ba120d2fbaf5be36426512
RyanDervisevic <ryan.dervisevic@gmail.com>
2020-11-08 20:24:43 +0100

    Video des trailer en Iframe

--- src/app/movi-details/movi-details.component.ts
+++ src/app/movi-details/movi-details.component.ts
@@ -2,6 +2,7 @@ import {  OnInit, Component } from '@angular/core';
 import { MoviesService } from '../services/movies.service';
 import { ActivatedRoute } from '@angular/router';
 import { SingleMovie, Video } from '../models/movie';
+import { DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   templateUrl: './movi-details.component.html',
@@ -14,13 +15,18 @@ export class MoviDetailsComponent implements OnInit {
   // added
   video: Video;
   imageBaseUrl = 'https://image.tmdb.org/t/p/original';
-
-  constructor( private movieService: MoviesService, private route: ActivatedRoute) { }
+  constructor( private sanitizer: DomSanitizer, private movieService: MoviesService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     const movieId: number = +this.route.snapshot.params.id;
     this.movieService.getMovie(movieId).subscribe(res => this.movie = res);
     // added
     this.movieService.getVideo(movieId).subscribe(res => this.video = res);
+  }
+  getUrl(key){
+    return 'https://www.youtube.com/watch?v=' + key;
+  }
+  getEmbedUrl(key){
+    return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + key + '?ecver=2')
   }
 }
